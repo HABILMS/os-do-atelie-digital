f6fa3c5aa3d7e2ba87fb1a9eb9bb027d8ced734d
HABILMS <88388546+HABILMS@users.noreply.github.com>
2025-05-24 23:33:55 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,1 +1,23 @@
+// server.cjs
+const express = require('express');
+const path = require('path');
+const { fileURLToPath } = require('url');
+const { dirname } = path;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+const distPath = path.join(__dirname, 'dist');
+
+app.use(express.static(distPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Servidor rodando em http://0.0.0.0:${PORT}`);
+});
